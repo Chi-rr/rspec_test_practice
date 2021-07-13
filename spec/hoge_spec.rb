27e466bf5e63a1fef5ae01acab5590c5bd752c10
Chihiro <Chi.r.r.333@gmail.com>
2021-07-13 11:07:19 +0900

    subjectでテストの主語を宣言し、itも省略。DRYにする

--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -3,22 +3,18 @@ require 'spec_helper'
 # describe class名
 RSpec.describe User do
   describe '#greet' do
-    let(:user) { User.new(params) } #2
-    let(:params) { { name: '花子', age: age } } #3
+    let(:user) { User.new(name: '花子', age: age) }
+    # subject = テストの主語。テストするものが一つに決まっている時に使用
+    subject { user.greet }
 
     context '12歳以下であったら' do
-      # letは遅延評価される(必要なタイミングで呼ばれる)
-      let(:age) { 12 } #4
-      it '語尾が「でーす！」になること' do
-        expect(user.greet).to eq '12でーす！' #1
-      end
+      let(:age) { 12 }
+      it { is_expected.to eq '12でーす！'  }
     end
 
     context '12歳より大きければ' do
       let(:age) { 13 }
-      it '語尾が「です」になること' do
-        expect(user.greet).to eq '13です'
-      end
+      it { is_expected.to eq '13です'}
     end
   end
 end
