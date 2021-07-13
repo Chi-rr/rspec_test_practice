b7d08673a64e0a267f85c9cf311c5ff001f28aa0
Chihiro <Chi.r.r.333@gmail.com>
2021-07-13 10:55:58 +0900

    ローカル変数もletにし、遅延評価を利用したテストに書き換える

--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -3,19 +3,20 @@ require 'spec_helper'
 # describe class名
 RSpec.describe User do
   describe '#greet' do
-    let(:params) { { name: '花子' } }
+    let(:user) { User.new(params) } #2
+    let(:params) { { name: '花子', age: age } } #3
 
-    # context = describeと機能としては同じだが「条件」をグループ化するのに使用する
     context '12歳以下であったら' do
+      # letは遅延評価される(必要なタイミングで呼ばれる)
+      let(:age) { 12 } #4
       it '語尾が「でーす！」になること' do
-        user = User.new(params.merge(age: 12))
-        expect(user.greet).to eq '12でーす！'
+        expect(user.greet).to eq '12でーす！' #1
       end
     end
 
     context '12歳より大きければ' do
+      let(:age) { 13 }
       it '語尾が「です」になること' do
-        user = User.new(params.merge(age: 13))
         expect(user.greet).to eq '13です'
       end
     end
