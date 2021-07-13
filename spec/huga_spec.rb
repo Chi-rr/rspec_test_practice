f403be187b6dd14ff98cb887bba8d2910a990bb7
Chihiro <Chi.r.r.333@gmail.com>
2021-07-13 15:47:22 +0900

    true/false/nil/整数/シンボルは「同じ値であれば同じインスタンス」のため、beもパスする

--- spec/huga_spec.rb
+++ spec/huga_spec.rb
@@ -4,13 +4,24 @@ RSpec.describe 'be/eqについて' do
   let(:color1) { 'red' }
   let(:color2) { 'red' }
   
-  # インスタンスが異なるので別物
-  it 'beを使うとcolor1 != color2' do
-    expect(color1).not_to be color2
+  context 'beを使う場合' do
+    # インスタンスが異なるので別物
+    it 'color1 != color2' do
+      expect(color1).not_to be color2
+    end
+
+    it '「同じ値であれば同じインスタンス」のためパスする' do
+      expect(true).to be true
+      expect(nil).to be nil
+      expect(1 + 1).to be 2
+      expect(:foo).to be :foo
+    end
   end
 
-  # インスタンスが異なっても、同値のためパスする
-  it 'eqを使うとcolor1 == color2' do
-    expect(color1).to eq color2
+  context 'eqを使う場合' do
+    # インスタンスが異なっても、同値のためパスする
+    it 'color1 == color2' do
+      expect(color1).to eq color2
+    end
   end
 end
