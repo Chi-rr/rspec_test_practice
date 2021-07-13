1730923ac83a86ee8c8de8dc263568aed9e480bd
Chihiro <Chi.r.r.333@gmail.com>
2021-07-13 10:07:15 +0900

    beforeでテスト実行前の共通処理を実行

--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -3,17 +3,21 @@ require 'spec_helper'
 # describe class名
 RSpec.describe User do
   describe '#greet' do
+    before do
+      @params = { name: '花子' }
+    end
+
     # context = describeと機能としては同じだが「条件」をグループ化するのに使用する
     context '12歳以下であったら' do
       it '語尾が「でーす！」になること' do
-        user = User.new(age: 12, name: 'はなこ')
+        user = User.new(@params.merge(age: 12))
         expect(user.greet).to eq '#{@age}でーす！'
       end
     end
 
     context '12歳より大きければ' do
       it '語尾が「です」になること' do
-        user = User.new(age: 13, name: '花子')
+        user = User.new(@params.merge(age: 13))
         expect(user.greet).to eq '#{@age}です'
       end
     end
