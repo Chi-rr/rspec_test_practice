d6832e60d08268c9a88140e359004c2eb51a5018
Chihiro <Chi.r.r.333@gmail.com>
2021-07-13 13:44:51 +0900

    example全体を実行したくない(skipしたい)場合

--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe User do
   let(:user) { User.new(name: '花子', age: age) }
 
   describe '#greet' do
-    subject { user.greet }
-
-    pending 'なぜか落ちてしまうテスト' do
-    #context '12歳より大きい' do
-      let(:age) { 12 }
-      it { is_expected.to eq '花子です' }
+    xit '意図的に実行したくない(skipしたい)テスト' do
+    # context '12歳より大きい' do
+      let(:age) { 13 }
+      it '語尾が「です」' do
+        expect(user.greet).to eq '花子です'
+      end
     end
   end
 end
