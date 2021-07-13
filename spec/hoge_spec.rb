2409f086126cd1f0400fee9ccfc57715ca42cf97
Chihiro <Chi.r.r.333@gmail.com>
2021-07-13 09:31:01 +0900

    context「条件」でグループ化

--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -3,14 +3,19 @@ require 'spec_helper'
 # describe class名
 RSpec.describe User do
   describe '#greet' do
-    it '12歳以下であったら、語尾が「でーす！」になること' do
-     user = User.new(age: 12, name: 'はなこ')
-     expect(user.greet).to eq '#{@age}でーす！'
+    # context = describeと機能としては同じだが「条件」をグループ化するのに使用する
+    context '12歳以下であったら' do
+      it '語尾が「でーす！」になること' do
+        user = User.new(age: 12, name: 'はなこ')
+        expect(user.greet).to eq '#{@age}でーす！'
+      end
     end
 
-    it '12歳より大きければ、語尾が「です」になること' do
-      user = User.new(age: 13, name: '花子')
-      expect(user.greet).to eq '#{@age}です'
+    context '12歳より大きければ' do
+      it '語尾が「です」になること' do
+        user = User.new(age: 13, name: '花子')
+        expect(user.greet).to eq '#{@age}です'
+      end
     end
   end
 end
