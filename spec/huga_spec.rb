dd43b6143933b68b9922adc89d36a173a2f5b8c3
Chihiro <Chi.r.r.333@gmail.com>
2021-07-13 16:22:19 +0900

    真偽値のテストにはbe_truthy/be_falseyを利用する

--- spec/huga_spec.rb
+++ spec/huga_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
-RSpec.describe 'be/eqについて' do
-  it 'be_emptyがempty?と同義である' do
-    expect([]).to be_empty
-    expect([].empty?).to eq true
+RSpec.describe '真偽値の確認' do
+  # ※Rybyは、false/nilは偽、それ以外は真
+  it 'be_truthy/be_falsey、テストはパスする' do
+    expect(1).to be_truthy
+    expect(nil).to be_falsey
+  end
+
+  it 'be true/alseの場合、テストは失敗する' do
+    expect(1).not_to be true
+    expect(nil).not_to be false
+  end
+
+  it 'eq true/falseの場合、テストは同じく失敗する' do
+    expect(1).not_to eq true
+    expect(nil).not_to eq false
   end
 end
