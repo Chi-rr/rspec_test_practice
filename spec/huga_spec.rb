57d08f3d69e93a779948d5c1a5b55b5083509de6
Chihiro <Chi.r.r.333@gmail.com>
2021-07-13 17:19:10 +0900

    raise_errorマッチャで、エラーを検証

--- spec/huga_spec.rb
+++ spec/huga_spec.rb
@@ -1,23 +1,7 @@
 require 'spec_helper'
 
-RSpec.describe 'includeマッチャ' do
-  it '配列内に含まれる値を確認' do
-    array = [1, 2, 3]
-    expect(array).to include 1, 2
-  end
-
-  it 'hash内に含まれる値を確認' do
-    hash = { :coffee => 100, :latte => 200, :special => { :moca => 300 } }
-    expect(hash).to include :coffee
-    expect(hash).to include :coffee => 100, :latte => 200
-    expect(hash).not_to include :moca => 300
-    expect(hash).not_to include :hoge
-  end
-
-  it '文字列に含まれる値を確認' do
-    string = 'hogefuga123'
-    expect(string).to include 'hoge'
-    expect(string).to include 'efu'
-    expect(string).not_to include 'hofu13'
+RSpec.describe 'raise_errorマッチャ' do
+  it 'エラーが起きることを確認' do
+    expect{ 1/0 }.to raise_error ZeroDivisionError
   end
 end
