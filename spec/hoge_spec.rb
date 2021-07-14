e9d6ca5ca4c8e44b2269c8cda14705517a2cce77
Chihiro <Chi.r.r.333@gmail.com>
2021-07-14 12:34:34 +0900

    モックの呼び出すメソッドの検証 expect(モックオブジェクト).to receive(メソッド)

--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -8,15 +8,13 @@ RSpec describe WeatherBot do
     #  expect{ weather_bot.tweet_forecast }.not_to raise_error
     #end
     it 'エラーなく、予報をツイートすること' do
-      # Twitter::REST::Clientのモックオブジェクト作成↓
-      # double(引数：任意) = モックオブジェクト作成
       twitter_client_mock = double('Twitter client')
-      # twitter_client.update できるようにしている↓
-      # allow(モックオブジェクト).to receive(メソッド名) = 呼び出し可能なメソッドを指定
-      allow(twitter_client_mock).to receive(:update)
+      #allow(twitter_client_mock).to receive(:update)
+
+      # こうすることで、updateメソッドが呼び出されたかも検証できる↓
+      expect(twitter_client_mock).to receive(:update)
 
       weather_bot = WeatherBot.new
-      # weather_botにtwitter_clientメソッドを呼び出されたら、twitter_client_mockを返すようにする
       allow(weather_bot).to receive(:twitter_client).and_return(twitter_client_mock)
 
       expect{ weather_bot.tweet_forecast }.not_to raise_error
