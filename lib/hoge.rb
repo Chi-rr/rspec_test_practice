04eee3f0730218be99dceb8e92ae97803b86c2d7
Chihiro <Chi.r.r.333@gmail.com>
2021-07-14 16:34:44 +0900

    どんなメソッドも許容するas_null_object

--- lib/hoge.rb
+++ lib/hoge.rb
@@ -1,11 +1,6 @@
 require 'twitter'　#gemが必要
 
 class WeatherBot
-  # 「天気」を検索し、はじめに返ってきたツイート文を返すメソッド
-  def serch_first_wether_tweet
-    twitter_client.serch('天気').first.text
-  end
-
   def tweet_forecast
     twitter_client.update '今日は晴れです'
   end
--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -2,24 +2,13 @@ require 'spec_helper'
 
 # describe class名
 RSpec describe WeatherBot do
-  it '天気を含む最初のツイートを返すこと' do
-    status_mock = double('Status')
-    allow(status_mock).to receive(:text).and_return('天気は曇り')
-
-    twitter_client_mock = double('TwitterClient')
-    allow(twitter_client).to receive(:serch).and_return([status_mock])
+  it 'エラーなく予報をツイートする' do
+    # as_null_object = どんなメソッドも許容する。戻り値はnull object
+    twitter_client_mock = double('Twitter client').as_null_object
+    # allow(twitter_client_mock).to receive(:update)
 
     weather_bot = WeatherBot.new
     allow(weather_bot).to receive(:twitter_client).and_return(twitter_client_mock)
-
-    expect(weather_bot.serch_first_wether_tweet).to eq '天気は曇り'
-  end
-
-  # 上記と同じテスト↑
-  it '天気を含む最初のツイートを返すこと(receive_message_chainを利用)' do
-    weather_bot = WeatherBot.new
-    allow(weather_bot).to receive_message_chain('twitter_client.search.first.text').and_return('天気は曇り')
-
-    expect(weather_bot.search_first_weather_tweet).to eq '天気は曇り'
+    expect{ weather_bot.tweet_forecast }.not_to raise_error
   end
 end
