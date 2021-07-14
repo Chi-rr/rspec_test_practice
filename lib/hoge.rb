2ae6e31c3581cec046784f72eb0d43d933ba72c6
Chihiro <Chi.r.r.333@gmail.com>
2021-07-14 12:50:24 +0900

    エラー処理のテストのためにモックを利用して、allow(モックオブジェクト).to receive(メソッド名).and_raise(エラー)でエラーを発生させる

--- lib/hoge.rb
+++ lib/hoge.rb
@@ -3,9 +3,15 @@ require 'twitter'　#gemが必要
 class WeatherBot
   def tweet_forecast
     twitter_client.update '今日は晴れです'
+  rescue => e
+    notify(e)
   end
 
   def twitter_client
     Twitter::REST::Client.new
+  end
+
+  def notify(error)
+    #error処理
   end
 end
--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -2,22 +2,27 @@ require 'spec_helper'
 
 # describe class名
 RSpec describe WeatherBot do
-  describe '#tweet_forecast' do
-    #it 'エラーなく、予報をツイートすること' do
-    #  weather_bot = WeatherBot.new
-    #  expect{ weather_bot.tweet_forecast }.not_to raise_error
-    #end
-    it 'エラーなく、予報をツイートすること' do
-      twitter_client_mock = double('Twitter client')
-      #allow(twitter_client_mock).to receive(:update)
-
-      # こうすることで、updateメソッドが呼び出されたかも検証できる↓
-      expect(twitter_client_mock).to receive(:update)
-
-      weather_bot = WeatherBot.new
-      allow(weather_bot).to receive(:twitter_client).and_return(twitter_client_mock)
-
-      expect{ weather_bot.tweet_forecast }.not_to raise_error
-    end
+  it 'エラーなく、予報をツイートすること' do
+    twitter_client_mock = double('Twitter client')
+    expect(twitter_client_mock).to receive(:update)
+    weather_bot = WeatherBot.new
+    allow(weather_bot).to receive(:twitter_client).and_return(twitter_client_mock)
+    expect{ weather_bot.tweet_forecast }.not_to raise_error
   end
+
+  it 'エラーが起こったら、通知する' do
+    twitter_client_mock = double('Twitter client')
+    # updateメソッドが呼ばれた時に、意図的にエラーを発生させる
+    expect(twitter_client_mock).to receive(:update).and_raise('エラーが発生しました')
+
+    weather_bot = WeatherBot.new
+    allow(weather_bot).to receive(:twitter_client).and_return(twitter_client_mock)
+    # エラーの発生を受け、notifyメソッドが呼ばれることを検証
+    expect(weather_bot).to receive(:notify)
+
+    # コードを実行する
+    # tweet_forecastメソッドを呼び出し、notifyメソッドが呼ばれればテストはパス
+    weather_bot.tweet_forecast
+  end
+
 end
