0d921cb3be8de862322994d4ce61922a8ea42316
Chihiro <Chi.r.r.333@gmail.com>
2021-07-15 11:12:40 +0900

    エクスペクテーションをまとめて実行し、実行速度を上げるデメリット(失敗するテスト全件報告されない)

---
+++ spec/fuga_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+RSpec.describe 'fuga' do
+  describe 'Slow spec' do
+    let(:fruits) { %w(apple orange melon) }
+    before do
+      puts "Preparing..."
+      sleep 2
+      puts "OK."
+    end
+
+    # 2つのエクスペクテーションが失敗するにも関わらず、1件しか報告されない
+    it 'has valid items' do
+      expect(fruits.size).to eq 2
+      expect(fruits.uniq).to contain_exactly(*fruits)
+      expect(fruits).to include 'tomato'
+    end
+
+    # 失敗する↓
+    # it 'has 3 items' do
+      # expect(fruits.size).to eq 2
+    # end
+
+    # 失敗する↓
+    # it 'does not have duplicated items' do
+      # expect(fruits.uniq).to contain_exactly(*fruits)
+    # end
+
+    # 成功する↓
+    # it 'contains apple' do
+      # expect(fruits).to include 'tomato'
+    # end
+  end
+end
--- spec/huga_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe 'raise_errorマッチャ' do
-  it 'エラーが起きることを確認' do
-    expect{ 1/0 }.to raise_error ZeroDivisionError
-  end
-end
