23eac6505897df0c20fafa484eb16830b1406857
Chihiro <Chi.r.r.333@gmail.com>
2021-07-15 11:49:52 +0900

    全テストでaggregate_failuresが有効にする(aggregate_failures: falseで無効化)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,10 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # 全テストでaggregate_failuresが有効にする(aggregate_failures: falseで無効化)
+  config.define_derivered_metadata do |meta|
+    meta[:aggregate_failures] = true unless meta.key?(:aggregate_failures)
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
