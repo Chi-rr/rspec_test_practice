1cf6522cf6698f93ca392aaeb52f3d895dcc6592
Chihiro <Chi.r.r.333@gmail.com>
2021-07-15 15:19:18 +0900

    % rspec 'spec/fuga_spec.rb' --only-failures で失敗したものだけ実行

--- spec/fuga_spec.rb
+++ spec/fuga_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe 'fuga' do
         expect(true).to be true
       end
       it 'ここは[1:2:2]' do
+        #fail 'わざと失敗させる'
         expect(true).to be true
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -57,9 +57,8 @@ RSpec.configure do |config|
   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   config.filter_run_when_matching :focus
 
-  # Allows RSpec to persist some state between runs in order to support
-  # the `--only-failures` and `--next-failure` CLI options. We recommend
-  # you configure your source control system to ignore this file.
+  # 失敗したテストだけを再実行できる--only-failuresを利用するため、失敗を検知するファイルを設定している↓
+  # このファイル内でstatusが「failed」のものだけを実行
   config.example_status_persistence_file_path = "spec/examples.txt"
 
   # Limits the available syntax to the non-monkey patched syntax that is
