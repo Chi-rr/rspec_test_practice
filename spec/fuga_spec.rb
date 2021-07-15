cb6495f49252a29a8f06f7f09caa8201bead423e
Chihiro <Chi.r.r.333@gmail.com>
2021-07-15 11:57:26 +0900

    IDを指定してグループを実行できる

--- spec/fuga_spec.rb
+++ spec/fuga_spec.rb
@@ -1,42 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe 'fuga' do
-  describe 'Slow spec' do
-    let(:fruits) { %w(apple orange melon) }
-    before do
-      puts "Preparing..."
-      sleep 2
-      puts "OK."
+  describe 'ここは[1]' do
+    it 'ここは[1:1]' do
+      expect(true).to be true
     end
-
-    it 'has valid items' do
-      aggregate_failures 'testing items' do
-        expect(fruits.size).to eq 2
-        expect(fruits.uniq).to contain_exactly(*fruits)
-        expect(fruits).to include 'tomato'
+    # rspec 'spec/fuga_spec.rb[1:2]'
+    describe 'ここは[1:2]' do
+      it 'ここは[1:2:1]' do
+        expect(true).to be true
+      end
+      it 'ここは[1:2:2]' do
+        expect(true).to be true
       end
     end
-
-    # 2つのエクスペクテーションが失敗するにも関わらず、1件しか報告されない
-    # it 'has valid items' do
-      # expect(fruits.size).to eq 2
-      # expect(fruits.uniq).to contain_exactly(*fruits)
-      # expect(fruits).to include 'tomato'
-    # end
-
-    # 失敗する↓
-    # it 'has 3 items' do
-      # expect(fruits.size).to eq 2
-    # end
-
-    # 失敗する↓
-    # it 'does not have duplicated items' do
-      # expect(fruits.uniq).to contain_exactly(*fruits)
-    # end
-
-    # 成功する↓
-    # it 'contains apple' do
-      # expect(fruits).to include 'tomato'
-    # end
   end
 end
