69cc78d9ad5404f9277ae698b3a2ecdb3497e0ce
Chihiro <Chi.r.r.333@gmail.com>
2021-07-16 11:01:22 +0900

    match_array/contain_exactly

--- spec/fuga_spec.rb
+++ spec/fuga_spec.rb
@@ -1,27 +1,13 @@
 require 'spec_helper'
 
-# このテストが必ずパスするようになった
-class Counter
-  def initialize
-    @mutex = Mutex.new
-    @count = 0
-  end
-
-  attr_reader :count
-
-  def increment
-    @mutex.synchronize { @count += 1 }
-  end
-end
-
-RSpec.describe Counter do
-  let(:counter) { Counter.new }
-
-  it 'increments the count in a threadsafe manner' do
-    threads = 10.times.map do
-      Thread.new { 1000.times { counter.increment } }
-    end
-    threads.each &:join
-    expect(counter.count).to eq 10_000
+RSpec.describe 'hoge' do
+  it '配列の個数と内容が正しいこと(順番は保証されない)' do
+    x = [2, 3, 1]
+    # 要素の個数、要素の同値性のみ検証し、配列の順番は検証されない(一つの配列を引数にとる)
+    expect(x).to match_array([1, 3, 2])
+    expect(x).to match_array [3, 2, 1]
+    
+    # 要素の個数、要素の同値性のみ検証し、配列の順番は検証されない(任意の数の引数を渡すことができる)
+    expect(x).to contain_exactly(1, 2, 3)
   end
 end
